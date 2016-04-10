1267cec7022331c99f7e8611ec98b8bcc2083f6b
MoonfireSeco <haaloplaysminecraft@gmail.com>
2016-04-10 18:02:10 +0300

    Added files via upload

---
+++ Tower of Magic/Asseturi/Scripturi/loadlvl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadlvl : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	public void load()
+    {
+        Application.LoadLevel("scene");
+    }
+}
