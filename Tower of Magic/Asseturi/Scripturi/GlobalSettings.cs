12dc004c141f1f81780e64d2cb7396713eec4be1
MoonfireSeco <haaloplaysminecraft@gmail.com>
2016-04-11 02:14:51 +0300

    Added files via upload

---
+++ Tower of Magic/Asseturi/Scripturi/GlobalSettings.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GlobalSettings : MonoBehaviour {
+
+    public static bool lefty=false;
+    public float movejoystickX;
+    public float movejumboX;
+    public float movecrouchbuttonY;
+
+    void Start ()
+    {
+        if (lefty && tag == "joystick")
+        {
+            gameObject.transform.localPosition = new Vector2(movejoystickX, gameObject.transform.localPosition.y);
+        }
+
+        if (lefty && tag == "jumbo-matic")
+        {
+            gameObject.transform.localPosition = new Vector2(movejumboX, gameObject.transform.localPosition.y);
+        }
+
+        if (lefty && tag == "crouchbutton")
+        {
+            gameObject.transform.localPosition = new Vector2(gameObject.transform.localPosition.x, movecrouchbuttonY);
+        }
+    }
+
+    public void changeLefty()
+    {
+        lefty = !lefty;
+    }
+
+    void Update()
+    {
+        if (tag == "switch")
+        {
+            //gameObject.SetActive(lefty);
+        }
+    }
+}
---
+++ Tower of Magic/Asseturi/Scripturi/gotomenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gotomenu : MonoBehaviour
+{
+
+    public GameObject source;
+    void Start()
+    {
+
+    }
+
+    public void load()
+    {
+        source.SetActive(true);
+        Application.LoadLevel("MainMenu");
+
+    }
+}
